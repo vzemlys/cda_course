ef248f2a4eeebd57732cfc3dfd056d74c68cf13c
Vaidotas Zemlys <zemlys@gmail.com>
2020-03-01 11:39:33 +0200

    Add the task 1

---
+++ doc/task1.R
@@ -0,0 +1,17 @@
+## Task
+
+##Load the data from the Covid data set.
+
+## 1. Select a random subsample of  China regions. Use function sample for that. The input of the task is a Covid data set. The output is the reduced Covid data set containing only the data for selected China regions.
+
+## 2. Convert the data to the long format. The example of long format can be found in https://github.com/vzemlys/cda_course/blob/master/data/long_format.csv. The date column should be a valid date. The input of the task is he Covid data set. The output is a data.frame in a long format with the date column as converted date.
+
+## 3. Select regions 1, 5 and 7 by alphabetical order.  Plot levels of cases in those regions in one graph. The x axis should be a date. Add labels for axes and the title. The input of the task is the data.frame from step 2. The output is one graph.
+
+## 4. Calculate the growths for these regions. Plot growths of those regions in one graph.The x axis should be a date. Add labels for axes and the title. The input of the task is the data.frame from the step 2. The outputs are the data.frame containing the growhts and the graph.
+
+##You can use package lubridate for working with dates. All other tasks should be done with base R. The result of the task should be R code which loads the data and performs the all the tasks. The code should run cleanly, i.e. should not produce any errors. All of the code should have comments (1, 2 sentences) explaining what are you trying to do. 
+
+## The weights of the subtasks for the final grade: 10%, 40%, 20%, 30%. 
+
+
